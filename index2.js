7f92e4ac6efe04e23c7fa67fd464463ba3942c39
Eljuancho871 <juandiegoramirez098@gmail.com>
2023-05-21 11:00:41 -0500

    docs:

--- index2.js
+++ index2.js
@@ -3,11 +3,11 @@ import * as THREE from "three";
 const $esfera_contenedor = document.querySelector("#esfera_contenedor");
 let esferas_array = [];
 
-var escena = new THREE.Scene();
+let escena = new THREE.Scene();
 escena.background = new THREE.Color(0x333333);
-var camara = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camara = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 const resize = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 };
